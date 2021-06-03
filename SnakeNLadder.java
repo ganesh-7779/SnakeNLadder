07febf17f9fb0fce9df42c346035ae6687170c10
Ganesh <ganeshgavhad9@gmail.com>
2021-06-03 15:20:00 +0530

    Player Position

---
+++ SnakeNLadder.java
@@ -0,0 +1,15 @@
+package snakeNLadder;
+
+import java.util.Random;
+
+public class SnakeNLadder {
+	
+	public static void main (String[]args) {
+		
+	}
+	
+	Random random = new Random();
+	int player = 0;
+	int WIN_POSITION=100;
+	
+	}
